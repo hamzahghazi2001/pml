faca10a037c2612f37c2f3475088e0d90a0cb0ac
Hamza Ghazi <77533830+hamzahghazi2001@users.noreply.github.com>
2025-06-17 13:27:03 +0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    domains: ['api.dicebear.com'],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+}
+
+export default nextConfig
